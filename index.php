90005631ed85356d05c0e698d9cb13ecf0528835
Susmoyrudra <92148495+Susmoyrudra@users.noreply.github.com>
2021-11-11 21:22:13 +0600

    Add files via upload

---
+++ index.php
@@ -0,0 +1,52 @@
+<?php
+ include '../view/layout/header.php'?>
+<!DOCTYPE html>
+<html>
+
+
+<body>
+
+    <div class="wrapper">
+        <!-- Sidebar Holder -->
+        <?php include '../view/layout/side_navbar.php' ?>
+
+        <!-- Page Content Holder -->
+        <div id="content">
+
+        <?php include '../view/layout/content_menu.php' ?>
+            
+        <form action="/action_page.php">
+     <div class="form-group">
+    <label for="email">Email address:</label>
+    <input type="email" class="form-control" id="email">
+   </div>
+  <div class="form-group">
+    <label for="pwd">password:</label>
+    <input type="password" class="form-control" id="pwd">
+  </div>
+  <div class="checkbox">
+    <label><input type="checkbox"> Remember me</label>
+  </div>
+  <button type="submit" class="btn btn-default">Submit</button>
+</form>
+        </div>
+    </div>
+
+    <!-- jQuery CDN - Slim version (=without AJAX) -->
+    <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
+    <!-- Popper.JS -->
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.0/umd/popper.min.js" integrity="sha384-cs/chFZiN24E4KMATLdqdvsezGxaGsi4hLGOzlXwp5UZB1LY//20VyM2taTB4QvJ" crossorigin="anonymous"></script>
+    <!-- Bootstrap JS -->
+    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.1.0/js/bootstrap.min.js" integrity="sha384-uefMccjFJAIv6A+rW+L4AHf99KvxDjWSu1z9VI8SKNVmz4sk7buKt/6v9KI65qnm" crossorigin="anonymous"></script>
+
+    <script type="text/javascript">
+        $(document).ready(function () {
+            $('#sidebarCollapse').on('click', function () {
+                $('#sidebar').toggleClass('active');
+                $(this).toggleClass('active');
+            });
+        });
+    </script>
+</body>
+
+</html>
